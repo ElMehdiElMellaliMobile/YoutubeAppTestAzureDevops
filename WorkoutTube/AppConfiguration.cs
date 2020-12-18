75df773dd3089ca7738dc82766abe126a364e7c3
ElMehdiElMellaliMobile <39305707+ElMehdiElMellaliMobile@users.noreply.github.com>
2020-12-18 18:09:44 +0100

    first commit

---
+++ WorkoutTube/AppConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkoutTube
+{
+    public class AppConfiguration : IAppConfiguration
+    {
+        public string GoogleApiBaseUrl => "https://www.googleapis.com";
+        public string YoutubeApiKey => "AIzaSyBMY67OriE7Qgx1XzLOGX5LtkhF9Z42xMc"; //Add your own API key
+        public string  VideosQuery => "FULL BODY WORKOUT";
+    }
+}
